cb63e2c662362fd53ee62f522f76d34148f87966
SuperToad <super-toad@hotmail.fr>
2017-11-01 20:06:03 +0100

    Edited for testing primBasic

--- devoir2.cpp
+++ devoir2.cpp
@@ -8,8 +8,13 @@ int main()
 	graph->addNode("sigurdowhen?!");
 	graph->addEdge("kek","ladylel",999);
 	graph->addEdge("kek","sigurdowhen?!",420);
+	graph->addEdge("ladylel","sigurdowhen?!",1000);
+	graph->addEdge("kek","kek",1000);
 	graph->showGraph();
 	cout << "Nombre d'arrêtes : " << graph->getEdgeCount() << endl;
+	
+	graph->primBasic();
+	
 	delete graph;
 	return 0;
 }
